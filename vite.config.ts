a8b15897107b8acf1580706fa0af7081aea7d649
DM <dervismartinez20@gmail.com>
2026-03-30 11:48:18 -0400

    fix:ajuste de carga de data en docker

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Redirige cualquier petición que empiece con /api al backend
+      '/api': {
+        target: 'http://localhost:3005',
+        changeOrigin: true,
+      },
+    },
+  },
 })
